c9754c1b20ff7c7afcd24d2132d111d117943f3f
mooosakhan <moosakhan3856902@gmail.com>
2025-06-15 01:56:39 -0700

    add 3000 port

--- src/server.js
+++ src/server.js
@@ -34,5 +34,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Server Error" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
